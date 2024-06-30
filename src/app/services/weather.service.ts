ae151b5ca5e560658d662e85047c1599c495646a
sambhav228 <sambhavkumar228@gmail.com>
2024-06-30 13:04:58 +0530

    webpack update

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -24,21 +24,19 @@ export class WeatherService {
 
 
 
-// private socket$  = webSocket('https://weatherapi-com.p.rapidapi.com/current.json');
-//   public messages$ = this.socket$.asObservable();
+private socket$  = webSocket('ws://localhost:4200/ws');
+  public messages$ = this.socket$.asObservable();
 
-//   public getWeatherData(cityName: any) {
-//     this.socket$.next(msg);
-//   }
 
-//   getWeatherData(cityName: string): Observable<WeatherData> {
-//     return this.http.get<WeatherData>('https://weatherapi-com.p.rapidapi.com/current.json', {
-//       headers: new HttpHeaders()
-//         .set('X-RapidAPI-Key', '40652ab04dmsh415785a9744e1ebp17f393jsn1647d51da4a7')
-//         .set('X-RapidAPI-Host', 'weatherapi-com.p.rapidapi.com'),
-//       params: new HttpParams()
-//         .set('q', cityName)
-//     })
-//   }
+
+  getWeatherUpdate(cityName: string): Observable<WeatherData> {
+    return this.http.get<WeatherData>('https://weatherapi-com.p.rapidapi.com/current.json', {
+      headers: new HttpHeaders()
+        .set('X-RapidAPI-Key', '40652ab04dmsh415785a9744e1ebp17f393jsn1647d51da4a7')
+        .set('X-RapidAPI-Host', 'weatherapi-com.p.rapidapi.com'),
+      params: new HttpParams()
+        .set('q', cityName)
+    })
+  }
 
 }
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /[\\/]node_modules[\\/]/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+}
